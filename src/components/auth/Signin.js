87facf28bda8eed1e9984f921b3c541bc73e57d5
unknown <ahmadabuawed@gmail.com>
2021-07-27 19:43:56 +0300

    add-forms

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,59 +1,65 @@
 import React from "react";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { signin } from "../../store/actions/authActions";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
-  const [user, setUser] = useState({
-    username: "",
-    password: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
   const history = useHistory();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(signin(user, history));
-  };
-
-  const handleChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+  const onSubmit = (data) => dispatch(signin(data, history));
 
   return (
-    <>
-      <h2>Signin</h2>
-      <hr />
-      <form onSubmit={handleSubmit}>
-        <div style={{ width: "350px" }} className="form-group">
-          <input
-            type="text"
-            className="form-control in"
-            placeholder="enter username"
-            onChange={handleChange}
-            name="username"
-            value={user.username}
-            required
-          />
-        </div>
-        <div style={{ width: "350px" }} className="form-group">
-          <input
-            type="password"
-            className="form-control in"
-            placeholder="enter your password"
-            onChange={handleChange}
-            name="password"
-            value={user.password}
-            required
-          />
-        </div>
-        <center>
-          <button type="submit" className="btn btn-dark">
-            Submit
-          </button>
-        </center>
-      </form>
-    </>
+  
+
+    <center>
+      
+    <div className="home">
+       <h1> Welcome To The Light Speed Chat </h1>
+      </div>
+        
+   
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+    <h2>Signin</h2><hr style={{width:"35%"}}/>
+     <input
+     placeholder="enter username"
+       id="username"
+       type="text"
+       className="form-control in"
+       {...register("username", { required: true })}
+     />
+     {errors.username && errors.username.type === "required" && (
+       <span role="alert">you have to enter your username</span>
+     )}
+     <input
+     placeholder="enter password"
+       id="password"
+       type="password"
+       className="form-control in"
+       {...register("password", { required: true })}
+     />
+     {errors.password && errors.password.type === "required" && (
+       <span role="alert">you have to enter your password</span>
+     )}
+     <br/>
+     <button type="submit" className="btn btn-dark">
+         Submit
+        </button>
+        <p>
+   <Link className="link" to="/signup">
+         signup
+       </Link>{" "}
+       if you don't have an account
+       </p>
+   </form>
+   
+   </center>
   );
 };
 
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,104 +1,76 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { signup } from "../../store/actions/authActions";
+import { useForm } from "react-hook-form";
 
 
 const Signup = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => dispatch(signup(data, history));
 
-  const [user, setUser] = useState({
-    fullname: "",
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const resetForm = () => {
-    setUser({
-      fullname: "",
-      username: "",
-      email: "",
-      password: "",
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(signup(user, history));
-    resetForm();
-  };
-
-  const handleChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+ 
+ 
   return (
-    <div>
-      <h2 >Signup</h2>
-      <hr/>
-      <form onSubmit={handleSubmit}>
-        <div style={{ width: "350px" }} className="form-group">
-        
-          <input
-            type="text"
-            className="form-control in"
-          
-            placeholder="enter full name"
-            onChange={handleChange}
-            name="fullname"
-            value={user.fullname}
-          />
-        </div>
-        <div style={{ width: "350px" }} className="form-group">
-         
-          <input
-            type="text"
-            className="form-control in"
-            
-            placeholder="enter username"
-            onChange={handleChange}
-            name="username"
-            value={user.username}
-            required
-          />
-        </div>
-        <div style={{ width: "350px" }} className="form-group">
-         
-          <input
-            type="email"
-            className="form-control in"
-           
-            placeholder="enter your email"
-            onChange={handleChange}
-            name="email"
-            value={user.email}
-            required
-          />
-        </div>
-
-        <div style={{ width: "350px" }} className="form-group">
-         
-
-          <input
-            type="password"
-            className="form-control in"
-           
-            placeholder="enter password"
-            onChange={handleChange}
-            name="password"
-            value={user.password}
-            required
-          />
-        </div>
-        <center>
-
+   <center>
+    <div className="home">
+    <h1> Welcome To The Light Speed Chat </h1>
+   </div>
+      
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <h2>Signup</h2><hr style={{width:"35%"}}/>
+      <input
+       placeholder="enter full name"
+      id="fullname"
+      type="text"
+      className="form-control in"
+      {...register("fullname", { required: true })}
+    />
+    {errors.fullname && errors.fullname.type === "required" && (
+      <span role="alert">you have to enter your fullname</span>
+    )}
+    <input
+     placeholder="enter username"
+      id="username"
+      type="text"
+      className="form-control in"
+      {...register("username", { required: true })}
+    />
+    {errors.username && errors.username.type === "required" && (
+      <span role="alert">you have to enter your username</span>
+    )}
+     <input
+     placeholder="enter your email"
+      id="email"
+      type="email"
+      className="form-control in"
+      {...register("email", { required: true })}
+    />
+    {errors.email && errors.email.type === "required" && (
+      <span role="alert">you have to enter your email</span>
+    )}
+    <input
+    placeholder="enter password"
+      id="password"
+      type="password"
+      className="form-control in"
+      {...register("password", { required: true })}
+    />
+    {errors.password && errors.password.type === "required" && (
+      <span role="alert">you have to enter your password</span>
+    )}
+        <br/>
         <button type="submit" className="btn btn-dark">
           Submit
-        </button>
-        </center>
-      </form>
-    </div>
+         </button>
+  </form>
+    </center>
+ 
   );
 };
 
